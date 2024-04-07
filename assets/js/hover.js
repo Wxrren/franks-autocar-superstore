a8bc9d6cf4447eb468745514aa2f5f81162c837a
Warren smith <warren.lee.smith@hotmail.co.uk>
2024-04-07 20:05:22 +0100

    ADD: Hover functions - change background color and font size

---
+++ assets/js/hover.js
@@ -0,0 +1,29 @@
+function saleHover() {
+    document.getElementById("sale-page").style.fontSize = "150%";
+    document.getElementById("sale-page").style.backgroundColor = "red";
+}
+
+function saleNormal() {
+    document.getElementById("sale-page").style.fontSize = "100%";
+    document.getElementById("sale-page").style.backgroundColor = "lightskyblue";
+}
+
+function soldHover() {
+    document.getElementById("sold-page").style.fontSize = "150%";
+    document.getElementById("sold-page").style.backgroundColor = "red";
+}
+
+function soldNormal() {
+    document.getElementById("sold-page").style.fontSize = "100%";
+    document.getElementById("sold-page").style.backgroundColor = "lightskyblue";
+}
+
+function contactHover() {
+    document.getElementById("contact-page").style.fontSize = "150%";
+    document.getElementById("contact-page").style.backgroundColor = "red";
+}
+
+function contactNormal() {
+    document.getElementById("contact-page").style.fontSize = "100%";
+    document.getElementById("contact-page").style.backgroundColor = "lightskyblue";
+}
