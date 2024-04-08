451939bd8ac4e918f60be0ba669ffdbc5c2ed999
Wxrren <warren.lee.smith@hotmail.co.uk>
2024-04-08 12:18:51 +0100

    ADD: Hover function to browser our stock button

--- assets/js/hover.js
+++ assets/js/hover.js
@@ -1,3 +1,5 @@
+
+// For sale navbar section
 function saleHover() {
     document.getElementById("sale-page").style.fontSize = "150%";
     document.getElementById("sale-page").style.backgroundColor = "red";
@@ -8,6 +10,7 @@ function saleNormal() {
     document.getElementById("sale-page").style.backgroundColor = "";
 }
 
+//Previously sold navbar section
 function soldHover() {
     document.getElementById("sold-page").style.fontSize = "150%";
     document.getElementById("sold-page").style.backgroundColor = "red";
@@ -18,6 +21,7 @@ function soldNormal() {
     document.getElementById("sold-page").style.backgroundColor = "";
 }
 
+// Contact us navbar section
 function contactHover() {
     document.getElementById("contact-page").style.fontSize = "150%";
     document.getElementById("contact-page").style.backgroundColor = "red";
@@ -26,4 +30,15 @@ function contactHover() {
 function contactNormal() {
     document.getElementById("contact-page").style.fontSize = "100%";
     document.getElementById("contact-page").style.backgroundColor = "";
+}
+
+// Browse our stock button
+function buttonHover() {
+    document.getElementById("browse-button").style.fontSize = "150%";
+    document.getElementById("browse-button").style.backgroundColor = "red";
+}
+
+function buttonNormal() {
+    document.getElementById("browse-button").style.fontSize = "100%";
+    document.getElementById("browse-button").style.backgroundColor = "";
 }
